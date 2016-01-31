67d267b3ac734d067512c1912ab8d580b95a6a47
olayakout <ola_yakout@yahoo.com>
2016-01-31 13:54:25 +0200

    Allow  users to save a filter

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'root';
+	$dbPassword = '';
+	$dbDatabase = 'demo_app';
+
+	$adminConfig = array(
+		'adminUsername' => "ola yakout",
+		'adminPassword' => "4ef4d3b296aab9434da77e423cff3b8f",
+		'notifyAdminNewMembers' => "",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Full Name",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "State",
+		'MySQLDateFormat' => "%m/%d/%Y",
+		'PHPDateFormat' => "n/j/Y",
+		'PHPDateTimeFormat' => "m/d/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "ola.yakout@gmail.com",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\n\nYour membership is now approved by the admin. You can log in to your account here:\nhttp://localhost/demo\n\nRegards,\nAdmin",
+		'hide_twitter_feed' => ""
+	);
